25ec578360f08124ef44ce0f41e47b62235ee3be
דניאלה סקומס <u8763623@bsmch.net>
2023-08-09 17:58:05 +0300

    hezita

---
+++ hezita/hezita/src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import Buttons from './components/Buttons';
+import './App.css';
+
+const App = () => {
+  return (
+    <div className='App'>
+      <Buttons />
+
+    </div>
+  );
+};
+
+export default App;
